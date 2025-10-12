20a862bbb20cd454ad332acdd277084846f3ef8c
Roshan Ravi <roshan@roshn.me>
2025-10-12 12:20:41 +0530

    BBG-Examples
    Temporary files - will be deleted in a month

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    // // select the buttons
+    // buttons = $('.hero-slider .flickity-button');
+    // dots = $('.hero-slider .flickity-page-dots');
+
+    // // add them to a newly created div
+    // slidernav = $('<div class="slider-navigation"></div>').append(buttons).append(dots)
+
+    // // append the new div (my-div) to .main-carousel
+    // $('.hero-slider').append(slidernav)
+})
